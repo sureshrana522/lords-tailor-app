2db9d4e08f0fd729791bf2dddbbe09a276890df4
sureshrana522 <sureshranasureshrana522@gmail.com>
2025-12-16 23:00:34 +0530

    Create Layout.tsx/src/index.css doesn't exist at build time

---
+++ src/components/Layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
